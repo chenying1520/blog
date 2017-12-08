07fb3f52d1b2fb28f504146833938e38fca6d88e
chenying <chenying@lyancoffee.com>
2017-12-08 13:16:14 +0800

    web demo

---
+++ spring-boot-demo/src/main/java/com/cheny/config/WebAppConfig.java
@@ -0,0 +1,36 @@
+
+package com.cheny.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+
+/**
+ * Created by chenying on 2017/12/7.
+ */
+
+@Configuration
+public class WebAppConfig {
+
+    /**
+     * @return ViewResolver接口声明了resolverViewName方法，这个方法的主要功能是根据ModelAndView中给定的viewName信息，再结合相关的配置，创建出合适类型的View对象
+     */
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+}
+
