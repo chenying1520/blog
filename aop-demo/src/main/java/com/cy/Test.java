fa1e19d4b0a7cbcc790b016eb68522f16c0b76a3
chenying <chenying@lyancoffee.com>
2018-04-02 10:43:44 +0800

    git测试-初始值

---
+++ aop-demo/src/main/java/com/cy/Test.java
@@ -0,0 +1,11 @@
+package com.cy;
+
+/**
+ * Created by chenying on 2018/1/18.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("我是初始值");
+    }
+
+}
